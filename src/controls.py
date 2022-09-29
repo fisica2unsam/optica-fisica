584a5aa749301926f26ebd6501b422c10945afbd
sstandre <simon.saint.a@gmail.com>
2022-09-29 16:24:53 -0300

    agregar controles a la red de difraccion

--- src/controls.py
+++ src/controls.py
@@ -1,5 +1,4 @@
 import numpy as np
-import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.ticker as plticker
 from matplotlib.colors import LinearSegmentedColormap
@@ -8,20 +7,21 @@ from functions import difraccion, interferencia, wavelength_to_rgb, int_dif
 
 from pyodide import create_proxy
 
+def num(n):
+    try:
+        x = int(n)
+    except ValueError:
+        x = float(n)
+    return x
+
+LOC = plticker.MultipleLocator(base=0.05) # this locator puts ticks at regular intervals
+
 def setup_1():
     x_lim = 0.4
     x = np.linspace(-x_lim, x_lim, 100000)
     return x,x_lim
 
 def plot_1(fig, ax, lam1, lam2, L, a, x, x_lim):
-
-    lam1 = int(lam1)
-    lam2 = int(lam2)
-    Element("L1_txt").write(f"L = {L:.2f} m")
-    Element("a1_txt").write(f"a = {a:.2f} μm")
-    Element("lam1_txt").write(f"λ₁ = {lam1} nm")
-    Element("lam2_txt").write(f"λ₂ = {lam2} nm")
-
     ax.clear()    
 
     y = difraccion(x, lam1*1e-9, L, a*1e-6)
@@ -31,8 +31,7 @@ def plot_1(fig, ax, lam1, lam2, L, a, x, x_lim):
     ax.plot(x,y, color=color1, label=f"$\lambda$ = {lam1} nm")
     ax.plot(x,y2, color=color2, label=f"$\lambda$ = {lam2} nm")
 
-    loc = plticker.MultipleLocator(base=0.05) # this locator puts ticks at regular intervals
-    ax.xaxis.set_major_locator(loc)
+    ax.xaxis.set_major_locator(LOC)
     ax.set_ylim(0,1)
     ax.set_xlim(-x_lim, x_lim)
     # ax.set_xlabel('sen(θ)')
@@ -47,7 +46,11 @@ input_elements = document.getElementsByName("params1")
 
 @create_proxy
 def change_lambda(event):
-    L, a, lam1, lam2 = [float(el.value) for el in input_elements]
+    L, a, lam1, lam2 = [num(el.value) for el in input_elements]
+    Element("L1_txt").write(f"L = {L:.2f} m")
+    Element("a1_txt").write(f"a = {a:.2f} μm")
+    Element("lam1_txt").write(f"λ₁ = {lam1} nm")
+    Element("lam2_txt").write(f"λ₂ = {lam2} nm")
     plot_1(fig, ax, lam1, lam2, L, a, *const_1)
 
 for ele in input_elements:
@@ -64,14 +67,6 @@ def setup_2():
     return x, x_lim
 
 def plot_2(fig2, ax3, ax4, lam, N, L, a, d, x, x_lim):
-    
-    lam = int(lam)
-    Element("N2_txt").write(f"N = {N}")
-    Element("L2_txt").write(f"L = {L:.2f} m")
-    Element("a2_txt").write(f"a = {a:.2f} μm")
-    Element("d2_txt").write(f"d = {d} μm")
-    Element("lam2_txt").write(f"λ₂ = {lam} nm")
-
     ax3.clear()  
     ax4.clear()  
 
@@ -80,8 +75,7 @@ def plot_2(fig2, ax3, ax4, lam, N, L, a, d, x, x_lim):
 
     ax3.plot(x,y, color=color)
     ax3.margins(x=0)
-    loc = plticker.MultipleLocator(base=0.05) # this locator puts ticks at regular intervals
-    ax3.xaxis.set_major_locator(loc)
+    ax3.xaxis.set_major_locator(LOC)
     ax3.set_ylim(0,1)
     # ax3.set_xlabel('sen(θ)')
     ax3.set_ylabel('Intensidad')
@@ -97,6 +91,7 @@ def plot_2(fig2, ax3, ax4, lam, N, L, a, d, x, x_lim):
     ax4.imshow(image, cmap=cmap, aspect="auto", extent=[-x_lim, x_lim, -1,1] )
     # ax4.set_xlabel('sen(θ)')
     ax4.set_xlabel('x (m)')
+    ax4.xaxis.set_major_locator(LOC)
     ax4.yaxis.set_visible(False)
     Element("vizb").write(fig2)
 
@@ -105,7 +100,12 @@ input2_elements = document.getElementsByName("params2")
 
 @create_proxy
 def change_params2(event):
-    L, N, a, d, lam = [float(el.value) for el in input2_elements]
+    L, N, a, d, lam = [num(el.value) for el in input2_elements]
+    Element("N2_txt").write(f"N = {N}")
+    Element("L2_txt").write(f"L = {L:.2f} m")
+    Element("a2_txt").write(f"a = {a:.2f} μm")
+    Element("d2_txt").write(f"d = {d} μm")
+    Element("lam2_txt").write(f"λ₂ = {lam} nm")
     plot_2(fig2, ax3, ax4, lam, N, L, a, d, *const_2)
 
 for ele in input2_elements:
@@ -122,30 +122,60 @@ plot_2(fig2, ax3, ax4, 600, 5, 1, 4, 30, *const_2)
 ##############
 
 
-fig3, ax5 = plt.subplots(layout='tight')
+# fig3, ax5 = plt.subplots(layout='tight')
 
 
-L = 1
-N = 3000        # lineas/cm
-d = 1/N *1e-2   # metros
-# a = d/10
-l_inf = 480 *1e-9
-l_sup = 650 *1e-9
-
-x_lim = 3.2*l_sup*L/d
-x = np.linspace(-x_lim, x_lim, 100_000)
+# L = 1
+# N = 3000        # lineas/cm
+# d = 1/N *1e-2   # metros
+# # a = d/10
+# l_inf = 480 *1e-9
+# l_sup = 650 *1e-9
+def setup_3():
+    # x_lim = 3.2*l_sup*L/d
+    x_lim = 0.6
+    x = np.linspace(-x_lim, x_lim, 40_000)
+    return x, x_lim
 
 # wv = 440 *1e-9
 # waves = np.linspace(wv, wv*1.01, 40)
 
-waves = np.linspace(l_inf, l_sup, 100)
-for wave in waves:
-    i = interferencia(x, wave, L, d, N)
-    ax5.plot(x, i, label=f'{wave*1e9:.0f} nm', color=wavelength_to_rgb(wave*1e9))
+def plot_3(fig3, ax5, l_inf, l_sup, N, L, d, x, x_lim):
+    ax5.clear()
+    waves = np.linspace(l_inf, l_sup, 100)
+    for wave in waves:
+        i = interferencia(x, wave*1e-9, L, d*1e-6, N)
+        ax5.plot(x, i, color=wavelength_to_rgb(wave)) # label=f'{wave} nm',
 
-ax5.set_ylim(0.01,0.9)
-ax5.yaxis.set_visible(False)
-ax5.set_xlabel('x (m)')
-ax5.set_ylabel('Intensidad')
+    # ax5.xaxis.set_major_locator(LOC)
+    ax5.set_ylim(0.01,0.5)
+    ax5.yaxis.set_visible(False)
+    ax5.set_xlabel('x (m)')
+    ax5.set_ylabel('Intensidad')
 
-Element("vizc").write(fig3)
+    Element("vizc").write(fig3)
+
+
+input3_elements = document.getElementsByName("params3")
+
+@create_proxy
+def change_params3(event):
+    L, N, l_inf, l_ran = [num(el.value) for el in input3_elements]
+    d = 1e4/N # micrones
+    l_sup = l_inf + l_ran
+    Element("L3_txt").write(f"L = {L:.2f} m")
+    Element("N3_txt").write(f"{N} líneas/cm")
+    Element("d3_txt").write(f"d = {d:.2f} μm")
+    Element("lam31_txt").write(f"λ₁ = {l_inf} nm")
+    Element("lam32_txt").write(f"{l_ran} nm")
+    Element("lam33_txt").write(f"λ₂ = {l_sup} nm")
+    
+    plot_3(fig3, ax5, l_inf, l_sup, N, L, d, *const_3)
+
+for ele in input3_elements:
+    ele.addEventListener("change", change_params3)
+
+
+fig3, ax5 = plt.subplots(layout='tight')
+const_3 = setup_3()
+plot_3(fig3, ax5, 480, 680, 3000, 1, 10/3, *const_3)
